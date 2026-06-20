bf0c4441f43ebd6c2f575e8041c6cdbf7a0ca1f6
Aayush33-cell <aayushtiwary33@gmail.com>
2026-06-20 17:23:39 +0530

    Question 1

---
+++ practiceProblems/src/moveZerosToEnd.java
@@ -0,0 +1,27 @@
+public class moveZerosToEnd {
+
+    static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    static void moveZeroesToEnd(int[] arr){
+        int insertPos = 0,i = 0;
+        while (i <arr.length){
+            if (arr[insertPos]!=0) insertPos++;
+            if (arr[i] == 0) i++;
+            else {
+                swap(arr,insertPos,i);
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,9,2,0,4,3,0,2,0};
+        moveZeroesToEnd(arr);
+        for(int x:arr)
+            System.out.print(x+",");
+    }
+}
